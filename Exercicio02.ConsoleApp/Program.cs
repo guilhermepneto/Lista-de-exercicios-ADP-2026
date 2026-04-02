1aadc471f0f7711e2b3132d2d6ad52524b04f655
guilhermepneto <gui.pereiraneto@hotmail.com>
2026-04-02 20:05:24 -0300

    Add resolução

--- Exercicio02.ConsoleApp/Program.cs
+++ Exercicio02.ConsoleApp/Program.cs
@@ -3,3 +3,17 @@ O programa deve solicitar ao usuário:
     a. A quilometragem inicial do veículo no início da viagem.
     b. A quilometragem final ao término da viagem.
     c. A quantidade de combustível consumida durante a viagem (em litros).*/
+
+Console.Clear();
+
+Console.Write("Informe a quantidade de KM percorrida: ");
+double km = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Informe a quantidade de litros de combustível abastecidos: ");
+double combustivel = Convert.ToDouble(Console.ReadLine());
+
+double media = km / combustivel;
+
+Console.WriteLine($"A média realizada pelo carro foi de: {media:F1} km/l");
+
+return;
