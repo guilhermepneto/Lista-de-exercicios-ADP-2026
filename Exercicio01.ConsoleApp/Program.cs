f8174e945123e7d87249415878aa4a80fdf0fc77
guilhermepneto <gui.pereiraneto@hotmail.com>
2026-04-02 19:44:47 -0300

    Add exercicio 01

--- Exercicio01.ConsoleApp/Program.cs
+++ Exercicio01.ConsoleApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//1. Crie um programa para calcular o volume de uma caixa retangular
+
+Console.Clear();
+
+Console.Write("Digite o valor do comprimento: ");
+string comprimento = Console.ReadLine();
+int c = Convert.ToInt32(comprimento);
+
+Console.Write("Digite o valor da largura: ");
+string largura = Console.ReadLine();
+int l = Convert.ToInt32(largura);
+
+Console.Write("Digite o valor da altura: ");
+string altura = Console.ReadLine();
+int a = Convert.ToInt32(altura);
+
+int resultado = c * a * l;
+
+Console.WriteLine($"O volume da caixa é: {resultado:F2} cm³");
+
+return;
