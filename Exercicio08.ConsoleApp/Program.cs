42dc383181093ee2b6896ebb55abb0ba2c70d3b6
guilhermepneto <gui.pereiraneto@hotmail.com>
2026-04-03 00:18:22 -0300

    Add exercicio resolvido

---
+++ Exercicio08.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿// 8. Faça um algoritmo para receber um número qualquer e informar na tela se é par ou ímpar.
+
+Console.Clear();
+
+Console.Write("Informe o número que deseja verificar: ");
+string numero = Console.ReadLine();
+int num = Convert.ToInt32(numero);
+
+if (num %2 == 0)
+{
+    Console.WriteLine("O número é par!");
+}
+else
+{
+    Console.WriteLine("O número é ímpar");
+}
+
+return;
+
